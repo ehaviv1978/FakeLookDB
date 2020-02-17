a763befb76b64e8a16e9141ae46f5ece8f839ec6
ehaviv1978 <ehaviv@hotmail.com>
2020-02-17 10:50:53 +0200

    allPost store Procedure improved

--- Database1/dbo/Stored Procedures/allPosts.sql
+++ Database1/dbo/Stored Procedures/allPosts.sql
@@ -1,8 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[allPosts]
 AS
 BEGIN
-    SELECT 
-        *
-    FROM 
-        Posts
+    SELECT Posts.Id, Posts.Description,Posts.Image,Posts.Location,Posts.TimePosted,Users.FirstName,Users.LastName   
+    FROM  Posts
+    LEFT JOIN Users ON Posts.UserId=Users.Id;
 END;
