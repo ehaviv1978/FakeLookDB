a5b9de509f1dcaa2217efbb2bb84bd2346593801
dtiger1000@gmail.com <dtiger1000@gmail.com>
2020-02-23 15:35:13 +0200

    added get user picture to allpost stored procedure

--- Database1/dbo/Stored Procedures/allPosts.sql
+++ Database1/dbo/Stored Procedures/allPosts.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[allPosts]
 AS
 BEGIN
-    SELECT Posts.postId, Posts.description,Posts.picture,Posts.location.Lat 'lat', location.Long 'long',Posts.timePosted,Users.firstName,Users.lastName
+    SELECT Posts.postId, Posts.description,Posts.picture,Posts.location.Lat 'lat', location.Long 'long',Posts.timePosted,Users.firstName,Users.lastName,users.picture as userPic
     FROM  Posts
     LEFT JOIN Users ON Posts.userId=Users.userId;
 END;
