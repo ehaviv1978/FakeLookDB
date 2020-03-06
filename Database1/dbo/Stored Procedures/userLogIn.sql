731d76891ff048f29f6c9661a18afed3f4cdad3c
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-03-06 19:54:59 +0200

    password type error fixed

--- Database1/dbo/Stored Procedures/userLogIn.sql
+++ Database1/dbo/Stored Procedures/userLogIn.sql
@@ -1,8 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[userLogIn]
 	@email nvarchar(100)= '',
-	@passsowrd nvarchar(100)=''
+	@password nvarchar(100)=''
 	
 AS
 	SELECT * from Users
-	where email = @email AND password=@passsowrd;
+	where email = @email AND password=@password;
 RETURN 0
