9ee60e10c0c32e6b21835dfac9cf84e89d2f29fc
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-03-07 11:02:31 +0200

    getPostById store procidure add

---
+++ Database1/dbo/Stored Procedures/getPostById.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[getPostById]
+		@userId int,
+        @postId int
+AS
+BEGIN
+    SELECT top 1 Posts.postId, Posts.description,Posts.picture,Posts.location.Lat 'lat', 
+	Posts.location.Long 'long', Posts.timePosted,Users.firstName,Users.lastName,users.picture as userPic ,
+	(SELECT count (*)
+	FROM PostLikes
+	WHERE PostLikes.postId = Posts.postId) as postLikeAmount
+	,CASE
+WHEN EXISTS (
+  SELECT top 1*
+  FROM PostLikes
+  WHERE PostLikes.postId = Posts.postId
+  AND PostLikes.userId = @userId
+) THEN CAST(1 AS BIT)
+ELSE CAST(0 AS BIT) END
+  AS 'liked'
+    FROM  Posts
+    LEFT JOIN Users ON Posts.userId=Users.userId
+	where postId = @postId
+END;
