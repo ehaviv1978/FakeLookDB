e4a1649bc29410ad04c29c4a0ab61653e5168306
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-03-22 14:07:53 +0200

    comments like add

--- Database1/dbo/Stored Procedures/allComments.sql
+++ Database1/dbo/Stored Procedures/allComments.sql
@@ -1,9 +1,22 @@
 ﻿CREATE PROCEDURE [dbo].[allComments]
-@postId int
+@postId int,
+@userId int
 AS
 BEGIN
     SELECT 
-        c.commentContent,c.timeCommented,u.firstName,u.lastName,u.picture, u.userId
+        c.commentId,c.commentContent,c.timeCommented,u.firstName,u.lastName,u.picture, u.userId,
+		(SELECT count (*)
+	    FROM CommentLikes
+	    WHERE CommentLikes.commentId = c.commentId) as commentLikeAmount
+	    ,CASE
+        WHEN EXISTS (
+        SELECT top 1*
+        FROM CommentLikes
+        WHERE CommentLikes.commentId = c.commentId
+        AND CommentLikes.userId = @userId
+)       THEN CAST(1 AS BIT)
+        ELSE CAST(0 AS BIT) END
+        AS 'liked'
     FROM  
         Comments c
 		    LEFT JOIN Users u ON c.userId=u.userId
