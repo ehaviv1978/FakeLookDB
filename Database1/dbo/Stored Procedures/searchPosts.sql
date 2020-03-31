94a05fd0e440d540dc2365204026a6288a49b425
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-03-31 23:00:09 +0300

    search posts by comment tags add

--- Database1/dbo/Stored Procedures/searchPosts.sql
+++ Database1/dbo/Stored Procedures/searchPosts.sql
@@ -1,6 +1,15 @@
 ﻿CREATE PROCEDURE [dbo].[searchPosts]
 	@searchParam varchar(100)
 AS
-	select * from Posts
-	where CHARINDEX(@searchParam,description) !=0;
+	SELECT Posts.postId, Posts.description,Posts.picture
+	FROM  Posts
+	where CHARINDEX(@searchParam,STUFF((SELECT ','+ tagContent
+	FROM PostTags WHERE [postId] = Posts.postId FOR XML PATH('')),1,1,'')) !=0 OR
+		CHARINDEX(@searchParam,STUFF((SELECT ',' +
+        STUFF((SELECT ','+ tagContent
+	    FROM CommentTags WHERE [commentId] = c.commentId FOR XML PATH('')),1,1,'') AS commentTags
+    FROM  
+        Comments c
+	WHERE
+		c.postId = Posts.postId FOR XML PATH('')),1,1,'')) !=0;
 RETURN 0
