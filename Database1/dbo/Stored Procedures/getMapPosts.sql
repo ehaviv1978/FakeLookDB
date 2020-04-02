355c796c9840fcefa13f5d857c84e1ca313f1685
ehaviv1978 <55824563+ehaviv1978@users.noreply.github.com>
2020-04-02 08:24:09 +0300

    get map markers add

---
+++ Database1/dbo/Stored Procedures/getMapPosts.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[getMapPosts]
+	@minLat int = 0,
+	@maxLat int =0,
+	@minLong int =0,
+	@maxLong int =0
+AS
+	SELECT Posts.postId, Posts.description,Posts.picture,Posts.location.Lat 'lat', 
+	Posts.location.Long 'long', users.picture as userPic
+    FROM  Posts
+    LEFT JOIN Users ON Posts.userId=Users.userId
+	where Posts.location.Lat > @minLat and Posts.location.Lat < @maxLat and
+	Posts.location.Long > @minLong and Posts.location.Long < @maxLong
+RETURN 0
